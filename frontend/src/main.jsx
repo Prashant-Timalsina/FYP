104c8ced07d64361133aec9a2c0dc4c6ce2ae61c
Prashant Timalsina <np03cs4a220102@heraldcollege.edu.np>
2025-01-01 21:55:35 +0545

    Added Login SignUp UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ShopContextProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </ShopContextProvider>
+  </BrowserRouter>
 );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,30 +1,75 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Login = () => {
+  const [currentState, SetCurrentState] = useState("Login");
+  const { navigate } = useContext(ShopContext);
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  const changeToHome = () => {
+    navigate("/");
+  };
   return (
-    <div>
-      <h1 className="text-center">Login</h1>
-      <form>
-        <div className="flex">
-          <label className="flex flex-col px-4">
-            First Name
-            <input
-              className="border rounded px-4 w-[100%]"
-              type="text"
-              placeholder="Write your name"
-            />
-          </label>
-          <label className="flex flex-col">
-            Last Name
-            <input
-              className="border rounded px-4 w-[100%]"
-              type="text"
-              placeholder="Write your name"
-            />
-          </label>
-        </div>
-      </form>
-    </div>
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border brder-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        type="email"
+        className="w-full px-3 py-2 border brder-gray-800"
+        placeholder="E-mail"
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border brder-gray-800"
+        placeholder="Password"
+        required
+      />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p>Forgot Password?</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => SetCurrentState("SignUp")}
+            className="cursor-pointer"
+          >
+            Create Account
+          </p>
+        ) : (
+          <p
+            onClick={() => SetCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+      <button
+        onClick={() => {
+          changeToHome();
+        }}
+        className="bg-black text-white font-light px-8 py-2 mt-4"
+      >
+        {currentState === "Login" ? "Login" : "SignUp"}
+      </button>
+    </form>
   );
 };
 
