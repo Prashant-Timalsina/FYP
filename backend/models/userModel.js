65d702e88cd81ebd84a667bcfbb0612aad074426
Prashant Timalsina <np03cs4a220102@heraldcollege.edu.np>
2025-01-03 13:23:43 +0545

    Added User Controller and user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+// Define the User Schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Enter User name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Enter email"],
+      unique: [true, "User email already in use"],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Enter Product price"],
+      minLength: [6, "Password should be more then 6 letters"],
+    },
+    cartData: { type: Object, default: {} },
+  },
+  {
+    minimize: false, // Keeps the empty fields as empty objects
+    timestamps: true, // Automatically adds `createdAt` and `updatedAt` fields
+  }
+);
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+export default userModel;
