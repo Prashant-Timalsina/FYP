fdfba81dbbed593cdd361554119610288e81c72c
Prashant Timalsina <np03cs4a220102@heraldcollege.edu.np>
2025-04-26 08:37:28 +0545

    Removed un necessary backends

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import sendEmail from "./middlewares/NodeMailer.js";
 import orderRouter from "./routes/orderRoutes.js";
 import favRouter from "./routes/favoriteRoutes.js";
 import bodyParser from "body-parser";
-import paymentRouter from "./routes/paymentRoutes.js";
+// import paymentRouter from "./routes/paymentRoutes.js";
 import chatRouter from "./routes/chatRoutes.js";
 
 import http from "http"; // Import HTTP module
@@ -42,7 +42,7 @@ app.use("/api/wood", woodRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/fav", favRouter);
-app.use("/api/payment", paymentRouter);
+// app.use("/api/payment", paymentRouter);
 
 app.use("/api/chat", chatRouter);
 
